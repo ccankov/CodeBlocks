21c23f08ff3b2b016ec4856e830f1c4ca239a801
Chris Cankov <cankov.chris@gmail.com>
2017-05-16 16:34:54 -0700

    Overhaul CSS file structure and style modal components

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   validates :email, :session_token, presence: true
   validates :email, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
 
   after_initialize :ensure_session_token
 
