009f781305ac3b9eb8b7fa30887a5d51df4e8a0e
Chris Cankov <cankov.chris@gmail.com>
2017-05-16 09:29:36 -0700

    Create Api/SessionsController and define auth actions

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,25 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(user_params)
+    if @user
+      render 'api/users/show'
+    else
+      render json: ["Invalid username or password"], status: 401
+    end
+  end
+
+  def destroy
+    if current_user
+      logout
+      render json: {}
+    else
+      render json: ["No logged in user"], status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
