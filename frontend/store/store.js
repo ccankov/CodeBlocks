64938ff5d6b3320b53048515c038e5364d125eff
Chris Cankov <cankov.chris@gmail.com>
2017-05-16 11:24:00 -0700

    Set up Redux devTools

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'remote-redux-devtools';
 import thunk from 'redux-thunk';
-
 
 import rootReducer from '../reducers/root_reducer';
 
+const composeEnhancers = composeWithDevTools({ realtime: true });
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk)
+    composeEnhancers(applyMiddleware(thunk))
   )
 );
 
