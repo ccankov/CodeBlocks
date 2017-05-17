1cb40c250beab09c07712308be3541ad00fcfa6b
Chris Cankov <cankov.chris@gmail.com>
2017-05-17 13:32:40 -0700

    Create home page footer and add some content to home articles

--- frontend/components/home/footer.jsx
+++ frontend/components/home/footer.jsx
@@ -3,7 +3,27 @@ import { Link } from 'react-router-dom';
 
 const Footer = (props) => (
   <footer className="nav-footer">
-    
+    <menu className="row">
+      <ul className="row">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/cvetomir-chris-cankov/">
+            About
+          </a>
+        </li>
+        <li><a>Contact</a></li>
+      </ul>
+    </menu>
+    <menu>
+      <p className="row">
+        <small>
+          CodeBlocks is a study platform that allows you to transcribe code
+          blocks into flashcards.<br />© 2017 Cvetomir Cankov
+        </small>
+      </p>
+    </menu>
   </footer>
 );
 
--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -8,16 +8,36 @@ const Home = (props) => (
     <NavbarContainer />
     <main>
       <article className="home-article article-1">
-
+        <h1 className="article-title">The World's Smartest Flashcards</h1>
+        <div className="scrolldown-container">
+          <i className="fa fa-chevron-down" aria-hidden="true"></i>
+        </div>
       </article>
       <article className="home-article article-2">
+        <section className="row">
+          <article className="transparent text-container">
 
+          </article>
+          <article className="text-container">
+
+          </article>
+        </section>
       </article>
       <article className="home-article article-3">
 
       </article>
       <article className="home-article article-4">
+        <div className="col">
+          <h2>Features</h2>
+          <section className="row">
+            <article className="text-container">
 
+            </article>
+            <article className="text-container">
+
+            </article>
+          </section>
+        </div>
       </article>
     </main>
     <Footer />
