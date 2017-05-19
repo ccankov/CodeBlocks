b090bf1555b24575b0cedbcb039aa434000b2ce3
Chris Cankov <cankov.chris@gmail.com>
2017-05-18 22:54:50 -0700

    Implement concept reducer

---
+++ frontend/reducers/concept_reducer.js
@@ -0,0 +1,21 @@
+import { RECEIVE_CONCEPTS, RECEIVE_CONCEPT, REMOVE_CONCEPT }
+  from '../actions/concept_actions';
+import { merge } from 'lodash';
+
+const conceptReducer = (state = {}, action) => {
+  let newState = Object.assign({}, state);
+  switch (action.type) {
+    case RECEIVE_CONCEPTS:
+      return action.concepts;
+    case RECEIVE_CONCEPT:
+      newState[action.concept.id] = action.concept;
+      return newState;
+    case REMOVE_CONCEPT:
+      delete newState[action.concept.id];
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default conceptReducer;
