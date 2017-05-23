68aaf76a1e3100b8d72584c4018e7aec507c2caf
Chris Cankov <cankov.chris@gmail.com>
2017-05-23 12:16:27 -0700

    Complete block form data updates, need to handle submit

--- frontend/components/library/block_form.jsx
+++ frontend/components/library/block_form.jsx
@@ -10,12 +10,19 @@ const _nullBlock = {
   prompt: '',
   public: false,
   language: {
-    id: 0,
-    name: ''
+    id: 1,
+    name: 'javascript'
   },
   output: '',
-  codeblock: null,
-  concepts: []
+  codeblock: {
+    allLines: [],
+    editLines: [],
+    editRanges: []
+  },
+  code: '',
+  concepts: [],
+  editor: null,
+  markerIds: []
 };
 
 class BlockForm extends React.Component {
@@ -26,6 +33,113 @@ class BlockForm extends React.Component {
     this.handleUpdate = this.handleUpdate.bind(this);
     this.handleConceptDelete = this.handleConceptDelete.bind(this);
     this.handleConceptAdd = this.handleConceptAdd.bind(this);
+    this.handleCodeChange = this.handleCodeChange.bind(this);
+    this.storeEditor = this.storeEditor.bind(this);
+    this.setupEditor = this.setupEditor.bind(this);
+    this.addEditLine = this.addEditLine.bind(this);
+    this.removeEditLine = this.removeEditLine.bind(this);
+    this.handleKeywordMark = this.handleKeywordMark.bind(this);
+    this.removeMarkers = this.removeMarkers.bind(this);
+  }
+
+  storeEditor(editor) {
+    this.setState({ editor }, () => this.setupEditor(editor));
+  }
+
+  addEditLine(idx) {
+    let { allLines, editLines, editRanges } = this.state.codeblock;
+    let editLinesSet = new Set(editLines);
+    editLinesSet.add(idx);
+    this.setState({
+      codeblock: {
+        allLines,
+        editLines: Array.from(editLinesSet),
+        editRanges
+      }
+    });
+  }
+
+  removeEditLine(idx) {
+    let { allLines, editLines, editRanges } = this.state.codeblock;
+    let editLinesSet = new Set(editLines);
+    editLinesSet.delete(idx);
+    this.setState({
+      codeblock: {
+        allLines,
+        editLines: Array.from(editLinesSet),
+        editRanges
+      }
+    });
+  }
+
+  removeMarkers(editor) {
+    let session = editor.getSession();
+    this.state.markerIds.forEach(markerId => session.removeMarker(markerId));
+  }
+
+  handleKeywordMark(e) {
+    e.preventDefault();
+    let editor = this.state.editor;
+    let range = editor.getSelection().getRange();
+    let startRow = range.start.row;
+    let startCol = range.start.column;
+    let endRow = range.end.row;
+    let endCol = range.end.column;
+    if ((startRow === endRow) && (startCol === endCol)) {
+      return;
+    }
+    let editRange = [startRow, startCol, endRow, endCol];
+    let session = editor.getSession();
+    let markerIds = this.state.markerIds;
+    let markerId = session.addMarker(range, "keyword", "keyword", false);
+    markerIds.push(markerId);
+    let { allLines, editLines, editRanges } = this.state.codeblock;
+    editRanges.push(editRange);
+    this.setState({
+      markerIds,
+      codeblock: {
+        allLines,
+        editLines,
+        editRanges
+      }
+    });
+  }
+
+  setupEditor(editor) {
+    editor.on("guttermousedown", e => {
+      var target = e.domEvent.target;
+      if (target.className.indexOf("ace_gutter-cell") === -1)
+          return;
+      if (!editor.isFocused())
+          return;
+      if (e.clientX > 25 + target.getBoundingClientRect().left)
+          return;
+      var breakpoints = e.editor.session.getBreakpoints(row, 0);
+      var row = e.getDocumentPosition().row;
+      if(typeof breakpoints[row] === typeof undefined) {
+        e.editor.session.setBreakpoint(row);
+        this.addEditLine(row);
+      }
+      else {
+        e.editor.session.clearBreakpoint(row);
+        this.removeEditLine(row);
+      }
+      e.stop();
+    });
+  }
+
+  handleCodeChange(newValue) {
+    let oldEditLines = this.state.codeblock.editLines;
+    this.removeMarkers(this.state.editor);
+    this.setState({
+      code: newValue,
+      codeblock: {
+        allLines: newValue.split('\n'),
+        editLines: oldEditLines,
+        editRanges: []
+      },
+      markerIds: []
+    });
   }
 
   handleUpdate(property) {
@@ -76,11 +190,16 @@ class BlockForm extends React.Component {
           <label className="floating-label">
             Step 1: Specify the programming language of your block
           </label>
-          <select
-            value={ this.state.language.id }
-            onChange={ this.handleUpdate('language') }>
-            { languageOptions }
-          </select>
+          <figure className="row language-select">
+            <select
+              value={ this.state.language.id }
+              onChange={ this.handleUpdate('language') }>
+              { languageOptions }
+            </select>
+            <p>
+              <i className="fa fa-caret-down" aria-hidden="true"></i>
+            </p>
+          </figure>
         </section>
         <section className="row input-container">
           <label className="floating-label">
@@ -108,12 +227,15 @@ class BlockForm extends React.Component {
             placeholder="Objective"
             onChange={ this.handleUpdate('prompt') }></input>
         </section>
-        <section className="col input-container">
+        <section className="col input-container align-start">
           <label className="floating-label">
             Step 4: Copy and mark up your code block
           </label>
-          <p>Identify key logic lines by clicking the gutter.
+          <p>Identify key logic lines to study by clicking the gutter.
             Select keywords and use the button to mark them for studying.</p>
+          <button className="slim-button" onClick={ this.handleKeywordMark }>
+            Mark Keyword
+          </button>
           <AceEditor
             mode={ this.state.language.name.length > 0
                     ? this.state.language.name : 'text' }
@@ -122,11 +244,14 @@ class BlockForm extends React.Component {
             fontSize={16}
             highlightActiveLine={true}
             focus={false}
+            onLoad={this.storeEditor}
+            onChange={this.handleCodeChange}
+            value={ this.state.code }
             style={{ width: "100%", height: "30vh" }}
             editorProps={{$blockScrolling: true}}
             />
         </section>
-        <section className="row input-container">
+        <section className="row input-container align-start">
           <label className="floating-label">
             Step 5: (Optional) Specify the expected output of the code block
           </label>
